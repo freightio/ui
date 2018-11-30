5777aaf45336bdbdd8e0974492dba806099c3dc0
Daniel Zhang <jmzwcn@gmail.com>
2018-11-30 21:41:20 +0800

    Confirm.
    Signed-off-by: Daniel Zhang <jmzwcn@gmail.com>

--- src/app/driver/driver.page.ts
+++ src/app/driver/driver.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 import { environment } from '../../environments/environment';
 import * as grpcWeb from 'grpc-web';
 import { OrdersClient } from '../../sdk/order_grpc_web_pb';
@@ -13,7 +14,10 @@ import { Order, OrderList } from '../../sdk/order_pb';
 })
 export class DriverPage implements OnInit {
   orders: any[];
-  constructor() {
+  ordersClient = new OrdersClient(environment.apiUrl, null, null);
+  constructor(
+    private alertController: AlertController,
+  ) {
     this.orders = [
       { 'name': '用户1', 'type': '小面包车', 'time': '2018-12-12,16:16', 'from': '三里屯', 'to': '天安门', 'fee': 66.66 },
       { 'name': '用户2', 'type': '大货车', 'time': '2018-12-12,18:18', 'from': '三里屯', 'to': '天安门', 'fee': 88.88 },
@@ -22,8 +26,7 @@ export class DriverPage implements OnInit {
   }
 
   ngOnInit() {
-    const ordersClient = new OrdersClient(environment.apiUrl, null, null);
-    ordersClient.list(new Order(), { 'custom-header-1': 'value1' },
+    this.ordersClient.list(new Order(), { 'custom-header-1': 'value1' },
       (err: grpcWeb.Error, response: OrderList) => {
         for (var i in response.getItemsList()) {
           let tsOrder = response.getItemsList()[i]
@@ -37,7 +40,7 @@ export class DriverPage implements OnInit {
           if (tsOrder.getTosList()[0] != null) {
             this.orders[i].to = tsOrder.getTosList()[0].toObject();
           }
-          this.orders[i].fee=tsOrder.getFee().toFixed(2);
+          this.orders[i].fee = tsOrder.getFee().toFixed(2);
           // let a = <Order>response.getItemsList()[i];
           // let order = new proto.backend.Order();
           // order.time = a.getId();
@@ -52,7 +55,29 @@ export class DriverPage implements OnInit {
       });
   }
 
-  showUserDetail(user) {
-    alert('正在开发中.' + user.name);
+  async showUserDetail(order) {
+    const alert = await this.alertController.create({
+      header: '确认接单',
+      buttons: [
+        {
+          text: '取消',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: () => {
+            console.log('Confirm Cancel');
+          }
+        }, {
+          text: '确定',
+          handler: data => {
+            this.ordersClient.update(new Order(), { 'custom-header-1': 'value1' },
+              (err: grpcWeb.Error, response: Order) => {
+                console.log(response);
+              });
+          }
+        }
+      ]
+    });
+    await alert.present();
   }
+
 }
