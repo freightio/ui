940e67d9fbfbeeaf999d523d932e51680c7d4a8b
Daniel <jmzwcn@gmail.com>
2018-12-09 11:48:43 +0800

    build.
    Signed-off-by: Daniel <jmzwcn@gmail.com>

--- src/app/driver/driver.page.ts
+++ src/app/driver/driver.page.ts
@@ -6,7 +6,7 @@ import * as grpcWeb from 'grpc-web';
 import { OrdersClient } from '../../sdk/order_grpc_web_pb';
 import { Order, OrderList, Position } from '../../sdk/order_pb';
 
-declare var proto;
+//declare var proto;
 declare var AMap;
 
 @Component({
@@ -23,57 +23,51 @@ export class DriverPage implements OnInit {
     private alertController: AlertController
   ) {
     this.orders = [
-      // { 'sender': { 'name': '用户1' }, 'type': '小面包车', 'created': '1543849950000', 'from': { 'name': '三里屯' }, 'to': { 'name': '天安门' }, 'fee': 66.66 },
-      // { 'sender': { 'name': '用户2' }, 'type': '大货车', 'created': '1543849950000', 'from': { 'name': '三里屯' }, 'to': { 'name': '天安门' }, 'fee': 88.88 },
-      // { 'sender': { 'name': '用户3' }, 'type': '中货车', 'created': '1543849950000', 'from': { 'name': '三里屯' }, 'to': { 'name': '天安门' }, 'fee': 99.99 }
+      { 'sender': { 'name': '用户1' }, 'type': '小面包车', 'created': '1543849950000', 'from': { 'name': '三里屯' }, 'to': { 'name': '天安门' }, 'fee': 66.66 },
+      { 'sender': { 'name': '用户2' }, 'type': '大货车', 'created': '1543849950000', 'from': { 'name': '三里屯' }, 'to': { 'name': '天安门' }, 'fee': 88.88 },
+      { 'sender': { 'name': '用户3' }, 'type': '中货车', 'created': '1543849950000', 'from': { 'name': '三里屯' }, 'to': { 'name': '天安门' }, 'fee': 99.99 }
     ];
-    // this.geolocation.getCurrentPosition().then((resp) => {
-    //   const positionInfo = [resp.coords.longitude + '', resp.coords.latitude + ''];
-    //   console.log(positionInfo);
-    //   //this.currentLocation = resp.coords;
-    //   this.ngOnInit();
-    // });
   }
 
   ngOnInit() {
-    this.geolocation.getCurrentPosition().then((resp) => {
-      // const positionInfo = [resp.coords.longitude + '', resp.coords.latitude + ''];
-      // console.log(positionInfo);
-      //this.currentLocation = resp.coords;
-      //this.ngOnInit();
-      let positon = new Position()
-      positon.setLocation(resp.coords.latitude + ',' + resp.coords.longitude);
-      this.ordersClient.list(new Position(), { 'custom-header-1': 'value1' },
-        (err: grpcWeb.Error, response: OrderList) => {
-          for (var i in response.getItemsList()) {
-            let tsOrder = response.getItemsList()[i]
-            // this.orders[i] = new proto.backend.Order()
-            this.orders[i] = tsOrder.toObject();
-            if (tsOrder.getSender() != null) {
-              this.orders[i].sender = tsOrder.getSender().toObject();
-            }
-            if (tsOrder.getFrom() != null) {
-              this.orders[i].from = tsOrder.getFrom().toObject();
-            }
-            if (tsOrder.getTosList()[0] != null) {
-              this.orders[i].to = tsOrder.getTosList()[0].toObject();
-            }
-            this.orders[i].fee = tsOrder.getFee().toFixed(2);
-            this.orders[i].annotations = new Map();
-            // let a = <Order>response.getItemsList()[i];
-            // let order = new proto.backend.Order();
-            // order.time = a.getId();
-            // order.contact = a.getContact();
-            // order.from = a.getFrom();
-            // order.tos = a.getTosList();
-            // order.type = a.getType();
-            // order.fee = a .getFee();
-            // this.orders[i] = order;
-          };
-          this.loadDistance();
-        });
-    });
+    //this.geolocation.getCurrentPosition().then((resp) => {
+    //  let positon = new Position()
+    //  positon.setLocation(resp.coords.latitude + ',' + resp.coords.longitude);
+    this.ordersClient.list(new Position(), { 'custom-header-1': 'value1' },
+      (err: grpcWeb.Error, response: OrderList) => {
+        for (var i in response.getItemsList()) {
+          let tsOrder = response.getItemsList()[i]
+          this.orders[i] = tsOrder.toObject();
+          if (tsOrder.getSender() != null) {
+            this.orders[i].sender = tsOrder.getSender().toObject();
+          }
+          if (tsOrder.getFrom() != null) {
+            this.orders[i].from = tsOrder.getFrom().toObject();
+          }
+          if (tsOrder.getTosList()[0] != null) {
+            this.orders[i].to = tsOrder.getTosList()[0].toObject();
+          }
+          this.orders[i].fee = tsOrder.getFee().toFixed(2);
+          // let a = <Order>response.getItemsList()[i];
+          // let order = new proto.backend.Order();
+          // order.time = a.getId();
+          // order.contact = a.getContact();
+          // order.from = a.getFrom();
+          // order.tos = a.getTosList();
+          // order.type = a.getType();
+          // order.fee = a .getFee();
+          // this.orders[i] = order;
+        };
+        this.loadDistance();
+      });
+    // });
+  }
 
+  refresh(event) {
+    this.ngOnInit();
+    setTimeout(() => {
+      event.target.complete();
+    }, 2000);
   }
 
   async showUserDetail(order) {
@@ -104,17 +98,18 @@ export class DriverPage implements OnInit {
     await alert.present();
   }
 
-
   loadDistance() {
     this.geolocation.getCurrentPosition().then(res => {
-      for (var i in this.orders) {
-        let order = this.orders[i];
+      for (var order of this.orders) {
         let p1 = ['' + res.coords.longitude, '' + res.coords.latitude]
         console.log('p1', p1);
         let p2 = order.from.location.split(',')
         console.log('p2', p2);
         const dis = AMap.GeometryUtil.distance(p1, p2);
         let ndis = Math.trunc(dis / 1000);
+        if (!order.annotations) {
+          order.annotations = new Map();
+        }
         order.annotations.set('distance', ndis);
       }
     }).catch(e => {
