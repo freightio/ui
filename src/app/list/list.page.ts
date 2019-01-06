bc3648137bc96e37a66aa0738d51a621075c8a0a
Daniel <jmzwcn@gmail.com>
2019-01-06 20:46:03 +0800

    navitate.
    Signed-off-by: Daniel <jmzwcn@gmail.com>

--- src/app/list/list.page.ts
+++ src/app/list/list.page.ts
@@ -109,25 +109,27 @@ export class ListPage implements OnInit {
   }
 
   navigate(order: any) {
-    var endLngLat = order.to.location.split(',');
-    let gaodeApp = startApp.set(
-      {
-        "action": "ACTION_VIEW",
-        "category": "CATEGORY_DEFAULT",
-        "type": "text/css",
-        "package": 'com.autonavi.minimap',
-        "uri": "androidamap://navi?sourceApplication=appname&poiname=" + "this.item.work_address" + "&lat=" + endLngLat[1] + "&lon=" + endLngLat[0] + "&dev=0",
-        "flags": ["FLAG_ACTIVITY_CLEAR_TOP", "FLAG_ACTIVITY_CLEAR_TASK"],
-        "intentstart": "startActivity",
-      }, { /* extras */
-        "EXTRA_STREAM": "extraValue1",
-        "extraKey2": "extraValue2"
-      }
-    );
-    gaodeApp.start(function () {
-      // alert('gaode ok')
-    }, function (error) {
-      alert(error)
-    })
+    if (window.confirm('开启导航?')) {
+      var endLngLat = order.to.location.split(',');
+      let gaodeApp = startApp.set(
+        {
+          'action': 'ACTION_VIEW',
+          'category': 'CATEGORY_DEFAULT',
+          'type': 'text/css',
+          'package': 'com.autonavi.minimap',
+          'uri': 'androidamap://navi?sourceApplication=appname&poiname=' + 'this.item.work_address' + '&lat=' + endLngLat[1] + '&lon=' + endLngLat[0] + '&dev=0',
+          'flags': ['FLAG_ACTIVITY_CLEAR_TOP', 'FLAG_ACTIVITY_CLEAR_TASK'],
+          'intentstart': 'startActivity',
+        }, { /* extras */
+          'EXTRA_STREAM': 'extraValue1',
+          'extraKey2': 'extraValue2'
+        }
+      );
+      gaodeApp.start(function () {
+        // alert('gaode ok')
+      }, function (error) {
+        alert(error)
+      })
+    }
   }
 }
